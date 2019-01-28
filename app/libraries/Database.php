8fd2907792d2b81648c305e7d7330a420029692b
khenryjoe <joseph.ketterer@gmail.com>
2019-01-28 11:09:11 +0000

    database

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -1,6 +1,7 @@
 <?php
 
 class Database {
+    // this info comes from config
     private $host = DB_HOST;
     private $user = DB_USER;
     private $pass = DB_PASS;
@@ -32,6 +33,45 @@ class Database {
 
     }
 
+    public function bind($param, $value, $type = null){
+        if(is_null(type)){
+            switch(true){
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+                    break;
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+
+        //perform prepare staemnt
+
+
+        }
+        public function execute(){
+            return $this-> stmt->execute();
+
+    }
+    // get results set as array of objects
+
+    public function resultsSet(){
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function single(){
+        $this->execute();
+        return $this ->stmt->fetch(PDO::FETCH_OBJ);
+    }
 
 
   }
