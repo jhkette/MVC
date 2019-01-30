f61712a2eb3b69d05aaa02d0b5801fc9f4737eb7
khenryjoe <joseph.ketterer@gmail.com>
2019-01-30 17:47:55 +0000

    posts adding from page

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -22,14 +22,59 @@ class Posts extends Controller {
 
     }
     public function add(){
-        $data=  [
 
-            'title' =>'',
-            'body' =>''
-        ];
+        if($_SERVER['REQUEST_METHOD']=='POST'){
 
-            $this->view('posts/add', $data);
-    }
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data=  [
+
+                'title' =>trim($_POST['title']),
+                'body' =>trim($_POST['body']),
+                'user_id' =>trim($_SESSION['user_id']),
+                'title_err' => '',
+                'body_err' => ''
+            ];
+
+
+            //validate title
+
+            if(empty($data['title'])){
+                $data['title_err'] = 'Please enter title';
+            }
+            if(empty($data['body'])){
+                $data['body_err'] = 'Please enter body text';
+            }
+
+
+            if(empty($data['title_err']) && empty($data['body_err'])) {
+                if($this -> postModel->addPost($data)){
+                flash('post_added', 'Post added');
+                redirect('posts');
+                }
+                else{
+                    die('broken');
+                }
+
+            }else{
+                $this->view('posts/add', $data);
+            }
+
+
+
+        }else{
+            $data=  [
+
+                'title' =>'',
+                'body' =>''
+            ];
+
+                $this->view('posts/add', $data);
+        }
+
+        }
+
+
+
 
 }
 
