1355f4b89a2804b83b30d896b23488e0eae51737
khenryjoe <joseph.ketterer@gmail.com>
2019-05-30 16:07:32 +0100

    remove composer

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,4 +1,6 @@
-
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+      <a class="navbar-brand" href="<?php echo URLROOT; ?>/users"">Community</a>
+</nav>      
 </div>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 <script src "<?php echo URLROOT; ?>/js/main.js>"> </script>
